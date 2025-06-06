a7365cc44ae41d0e5c9d2c2ef5801ba95633a4b1
Thilak T P <tpthilaktp@gmail.com>
2025-06-06 21:40:18 +0530

    Added separate classes to classify fetching data

--- forkbench-win32/forkbench-win32/fetch-details.h
+++ forkbench-win32/forkbench-win32/fetch-details.h
@@ -9,41 +9,108 @@ std::wstring GetWMIInfo(const std::wstring& wmiClass, const std::wstring& proper
 void collectWMIInfo(HWND hwnd);
 std::wstring decodeMemoryType(const std::wstring& typeCode);
 
-class outputDeviceInfo {
-	
-};
-class inputDeviceInfo {
+
+class Computer {
 
 };
+class powerInfo : public Computer {
+	std::wstring powerBatteryStatus;
+	std::wstring powerBatteryChargeRemaining;
+	std::wstring powerEstimatedRunTime;
+	std::wstring powerBatteryChemistry;
+	std::wstring powerElementName;
+	std::wstring powerIsActive;
+};
+class outputDeviceInfo : public Computer {
+	std::wstring soundDeviceName;
+	std::wstring soundDeviceManufacturer;
+	std::wstring soundDeviceStatus;
+};
+class inputDeviceInfo  : public Computer {
+	std::wstring inputDeviceName;
+	std::wstring inputDeviceID;
+	std::wstring inputDeviceNumberofFunctionKeys;
+	std::wstring inputPointingDeviceName;
+	std::wstring inputPointingDeviceID;
+	std::wstring inputPointingDeviceNumberofButtons;
+};
 
-class NetworkInfo {
+class NetworkInfo : public Computer {
+	std::wstring networkAdapterName;
+	std::wstring networkMACAddress;
+	std::wstring networkConnectionID;
+	std::wstring networkSpeed;
+	std::wstring networkAdapterType;
+	std::wstring networkAdapterDescription;
+	std::wstring networkAdapterIPAddress;
+	std::wstring networkDefaultGateway;
+	std::wstring networkDNSHostName;
+	std::wstring networkDHCPEnabled;
+};
+class OSInfo : public Computer {
+	std::wstring osName;
+	std::wstring osVersion;
+	std::wstring osBuildNumber;
+	std::wstring osUserName;
+	std::wstring osLastBootUpTime;
+	std::wstring osInstallDate;
+	std::wstring osRegisteredUser;
+	std::wstring OSArchitecture;
+	std::wstring osLocale;
+	std::wstring osSerialNumber;
+	std::wstring osSystemDrive;
+};
+class StorageInfo : public Computer {
+	std::wstring diskSize;
+	std::wstring diskType;
+	std::wstring diskModel;
+	std::wstring diskSerialNumber;
+	std::wstring diskInterfaceType;
+	std::wstring diskFirmwareVersion;
+	std::wstring diskPartition;
+	std::wstring diskDeviceID;
+	std::wstring diskFileSystem;
+	std::wstring diskVolumeName;
+	std::wstring diskLabel;
+	std::wstring diskDriveLetter;
+	std::wstring diskCapacity;
+	std::wstring diskFreeSpace;
+	std::wstring diskFileSystemType;
 
 };
-class OSInfo {
+class MotherboardInfo : public Computer {
+	std::wstring motherboardManufacturer;
+	std::wstring motherboardProductName;
+	std::wstring motherboardSerialNumber;
+	std::wstring motherboardBIOSVersion;
+	std::wstring motherboardReleaseDate;
+	std::wstring motherboardManufacturer;
 
 };
-class StorageInfo {
-
+class SystemMemoryInfo: public Computer {
+	std::wstring totalPhysicalMemory;
+	std::wstring RAMType;
+	std::wstring RAMSpeed;
+	std::wstring RAMFormFactor;
+	std::wstring RAMManufacturer;
+	std::wstring RAMPartNumber;
+	std::wstring RAMSerialNumber;
+	std::wstring RAMCapacity;
 };
-class MotherboardInfo {
-
-};
-class SystemMemoryInfo {
-
-};
-class GPUandDisplayInfo {
+class GPUandDisplayInfo : public Computer {
 	std::wstring gpuName;
 	std::wstring gpuDriverVer;
 	std::wstring gpuTotalVRAM;
-	std::wstring ;
-	std::wstring;
-	std::wstring;
-	std::wstring;
-	std::wstring;
-	std::wstring;
+	std::wstring gpuVideoModeDescription;
+	std::wstring gpuCurrentRefreshRate;
+	std::wstring gpuCurrentHResolution;
+	std::wstring gpuCurrentVResolution;
+	std::wstring gpuMonitorType;
+	std::wstring gpuScreenHeight;
+	std::wstring gpuScreenWidth;
 
 };
-class CPUInfo{
+class CPUInfo : public Computer {
 	std::wstring cpuName;
 	std::wstring cpuCodeName;
 	std::wstring cpuNumberOfCores;
@@ -58,6 +125,3 @@ class CPUInfo{
 	std::wstring cpuVoltageCapacity;
 };
 
-class Computer {
-	
-};
